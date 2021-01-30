4f3f2b7bb38478c03bc9d72bf2bab253af4182b0
Debouchage Antoine <antoine.debouchage@student-cs.fr>
2021-01-30 19:10:05 +0100

    Adding fitness class and static useful and basic fitness loss function

--- fitness.py
+++ fitness.py
@@ -13,3 +13,63 @@ def fitness_indiv(indiv, traj3D, dna_seq):
     return(lastVect.dot(lastVect))
 
 
+import math
+
+class Fitness:
+
+    @staticmethod
+    def squared_loss(predict, real):
+        loss = 0
+        length = len(predict)
+        if length != len(real):
+            raise ValueError('real and predict arguments do not have the same length')
+        for i in range(len(predict)):
+            loss += (real[i]-predict[i])**2
+        return loss/2
+    
+    @staticmethod
+    def absolute_loss(predict, real):
+        loss = 0
+        length = len(predict)
+        if length != len(real):
+            raise ValueError('real and predict arguments do not have the same length')
+        for i in range(len(predict)):
+            loss += abs(real[i]-predict[i])
+        return loss/2
+
+        
+    @staticmethod
+    def hinge_loss(predict, real):
+        loss = 0
+        length = len(predict)
+        if length != len(real):
+            raise ValueError('real and predict arguments do not have the same length')
+        for i in range(len(predict)):
+            loss += max(0, 1-predict[i]*real[i])
+        return loss/2
+    
+    
+    @staticmethod
+    def logistic_loss(predict, real):
+        loss = 0
+        length = len(predict)
+        if length != len(real):
+            raise ValueError('real and predict arguments do not have the same length')
+        for i in range(len(predict)):
+            loss += math.log(1+math.exp(-predict[i]*real[i]))
+        return loss/2
+    
+    
+    @staticmethod
+    def cross_entropy_loss(predict, real):
+        loss = 0
+        length = len(predict)
+        if length != len(real):
+            raise ValueError('real and predict arguments do not have the same length')
+        for i in range(len(predict)):
+            if predict[i] > 0 and predict[i] < 1:
+                loss += -real[i]*math.log(predict[i])-(1-predict[i])*math.log(1-predict[i])
+            else:
+                raise ValueError("predict must be between 0 and 1")
+        return loss/2
+    
