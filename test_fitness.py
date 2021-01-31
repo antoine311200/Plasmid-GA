48224c86f6c7e1ed948a0046d55d6467dccb603b
Debouchage Antoine <antoine.debouchage@student-cs.fr>
2021-01-31 21:19:01 +0100

    Change pytest for fitness

--- test_fitness.py
+++ test_fitness.py
@@ -1,23 +1,25 @@
 from fitness import *
 
 target = [1,2,3,4,5,6,7,8,9]
-
 sample = [2,4,1,5,3,8,1,9,5]
 
+target_entropy = [0.2,0.5,0.3,0.1,0.2]
+sample_entropy = [0.1,0.8,0.4,0.1,0.5]
+
 def test_squared_loss():
-    print(Fitness.squared_loss(sample, target))
+    assert Fitness.squared_loss(sample, target) == 35.5
 
 def test_absolute_loss():
-    print(Fitness.absolute_loss(sample, target))
+    assert Fitness.absolute_loss(sample, target) == 10.5
 
 def test_hinge_loss():
-    print(Fitness.hinge_loss(sample, target))
+    assert Fitness.hinge_loss(sample, target) == 0
 
 def test_logistic_loss():
-    print(Fitness.logistic_loss(sample, target))
+    assert Fitness.logistic_loss(sample, target) == 0.1767625434068119
 
 def test_cross_entropy_loss():
-    print(Fitness.cross_entropy_loss(sample, target))
+    assert Fitness.cross_entropy_loss(sample_entropy, target_entropy) == 2.3804694344400286
 
 test_squared_loss()
 test_absolute_loss()
